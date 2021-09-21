1a4d3eb2c23aef7b5d443b44ef6b90486202a443
AlbaqamiAziz <67348395+AlbaqamiAziz@users.noreply.github.com>
2021-09-21 21:32:51 +0300

    Days Of The Week Practices_1

---
+++ src/DaysDfTheWeek.kt
@@ -0,0 +1,7 @@
+fun main() {
+    val daysOfWeek = mapOf(1 to "Sunday", 2 to "Monday", 3 to "Tuesday", 4 to "wednesday", 5 to "Thursday", 6 to "Friday", 7 to "Saturday")
+    for (day in daysOfWeek){
+        println(day)
+    }
+
+}
