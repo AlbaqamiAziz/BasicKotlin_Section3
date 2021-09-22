bc6c154dceb2bb887f3d3896320b2848c311bc71
AlbaqamiAziz <67348395+AlbaqamiAziz@users.noreply.github.com>
2021-09-22 14:49:34 +0300

    Update

--- src/CountryCapitalsGame.kt
+++ src/CountryCapitalsGame.kt
@@ -10,26 +10,27 @@ import kotlin.random.Random
 //Start a new game if they want to play again
 fun main(){
      class Country(val name: String, val capital: String) {
-         fun info(){
+            fun info(){
              println("The capital of ${this.name} is the city of ${this.capital}")
          }
     }
 
+
     val countries = listOf<Country>(
         Country("Saudi Arabia","Riyadh"),
-        Country("Romania","Bucharest"),
+         Country("Romania","Bucharest"),
         Country("Spain","Barcelona"),
-        Country("United Arab Emirates","Abu Dhabi"),
+           Country("United Arab Emirates","Abu Dhabi"),
         Country("Japan","Tokyo"),
         Country("Ireland","Dublin"),
         Country("Greece","Athens"),
-        Country("Finland","Helsinki"),
+            Country("Finland","Helsinki"),
         Country("Estonia","Tallinn"),
         Country("Egypt","Cairo"),
     )
 
     do{
-        println("Guess the capitals of the following three countries ..")
+         println("Guess the capitals of the following three countries ..")
         var score = 0
         //Bonus:
         //Can you find a way to always ask 3 unique questions? (Never repeating the same country in a single game)
@@ -37,7 +38,7 @@ fun main(){
         var attempts=0
 
         while(attempts < 3){
-            val ranomNum = Random.nextInt(10)
+              val ranomNum = Random.nextInt(10)
             if (randomNumbers.contains(ranomNum)){
              continue
             }
@@ -47,11 +48,11 @@ fun main(){
             }else {
                 countries[ranomNum].info()
             }
-            attempts++
+              attempts++
             randomNumbers.add(ranomNum)
         }
         println("Your score $score out of 3 ..")
-        print("Do you want to play again ? (Y/N) : ")
+            print("Do you want to play again ? (Y/N) : ")
         val ans = readLine().toString()
     } while(ans == "Y" || ans == "y")
 
